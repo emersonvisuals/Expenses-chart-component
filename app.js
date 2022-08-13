d1bc508a6063b4087f89da6268d44dac35e63412
Thomas Emerson <emersonvisuals@gmail.com>
2022-08-13 15:43:48 +1000

    fetch data from JSON file

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ const fridayDiagram = document.querySelector('.fridayDiagram');
 const saturdayDiagram = document.querySelector('.saturdayDiagram');
 const sundayDiagram = document.querySelector('.sundayDiagram');
 
+const monData = document.querySelector('.monP');
+const tueData = document.querySelector('.tueP');
+const wedData = document.querySelector('.wedP');
+const thuData = document.querySelector('.thuP');
+const friData = document.querySelector('.friP');
+const satData = document.querySelector('.satP');
+const sunData = document.querySelector('.sunP');
+
 mondayDiagram.addEventListener('click', () => {
     mondayDiagram.classList.toggle('active');
+    monData.classList.toggle('active');
 
     function checkClicks() {
         if (tuesdayDiagram.classList.contains('active') || wednesdayDiagram.classList.contains('active') || thursdayDiagram.classList.contains('active') || fridayDiagram.classList.contains('active') || saturdayDiagram.classList.contains('active') || sundayDiagram.classList.contains('active')) {
@@ -19,6 +28,12 @@ mondayDiagram.addEventListener('click', () => {
             fridayDiagram.classList.remove('active');
             saturdayDiagram.classList.remove('active');
             sundayDiagram.classList.remove('active');
+            tueData.classList.remove('active');
+            wedData.classList.remove('active');
+            thuData.classList.remove('active');
+            friData.classList.remove('active');
+            satData.classList.remove('active');
+            sunData.classList.remove('active');
         } else {
             return
         }
@@ -29,6 +44,7 @@ mondayDiagram.addEventListener('click', () => {
 
 tuesdayDiagram.addEventListener('click', () => {
     tuesdayDiagram.classList.toggle('active');
+    tueData.classList.toggle('active');
 
     function checkClicks() {
         if (mondayDiagram.classList.contains('active') || wednesdayDiagram.classList.contains('active') || thursdayDiagram.classList.contains('active') || fridayDiagram.classList.contains('active') || saturdayDiagram.classList.contains('active') || sundayDiagram.classList.contains('active')) {
@@ -38,6 +54,12 @@ tuesdayDiagram.addEventListener('click', () => {
             fridayDiagram.classList.remove('active');
             saturdayDiagram.classList.remove('active');
             sundayDiagram.classList.remove('active');
+            monData.classList.remove('active');
+            wedData.classList.remove('active');
+            thuData.classList.remove('active');
+            friData.classList.remove('active');
+            satData.classList.remove('active');
+            sunData.classList.remove('active');
         } else {
             return
         }
@@ -47,6 +69,7 @@ tuesdayDiagram.addEventListener('click', () => {
 
 wednesdayDiagram.addEventListener('click', () => {
     wednesdayDiagram.classList.toggle('active');
+    wedData.classList.toggle('active');
 
     function checkClicks() {
         if (mondayDiagram.classList.contains('active') || tuesdayDiagram.classList.contains('active') || thursdayDiagram.classList.contains('active') || fridayDiagram.classList.contains('active') || saturdayDiagram.classList.contains('active') || sundayDiagram.classList.contains('active')) {
@@ -56,6 +79,12 @@ wednesdayDiagram.addEventListener('click', () => {
             fridayDiagram.classList.remove('active');
             saturdayDiagram.classList.remove('active');
             sundayDiagram.classList.remove('active');
+            monData.classList.remove('active');
+            tueData.classList.remove('active');
+            thuData.classList.remove('active');
+            friData.classList.remove('active');
+            satData.classList.remove('active');
+            sunData.classList.remove('active');
         } else {
             return
         }
@@ -65,6 +94,7 @@ wednesdayDiagram.addEventListener('click', () => {
 
 thursdayDiagram.addEventListener('click', () => {
     thursdayDiagram.classList.toggle('active');
+    thuData.classList.toggle('active');
 
     function checkClicks() {
         if (mondayDiagram.classList.contains('active') || tuesdayDiagram.classList.contains('active') || wednesdayDiagram.classList.contains('active') || fridayDiagram.classList.contains('active') || saturdayDiagram.classList.contains('active') || sundayDiagram.classList.contains('active')) {
@@ -74,6 +104,12 @@ thursdayDiagram.addEventListener('click', () => {
             fridayDiagram.classList.remove('active');
             saturdayDiagram.classList.remove('active');
             sundayDiagram.classList.remove('active');
+            monData.classList.remove('active');
+            tueData.classList.remove('active');
+            wedData.classList.remove('active');
+            friData.classList.remove('active');
+            satData.classList.remove('active');
+            sunData.classList.remove('active');
         } else {
             return
         }
@@ -83,6 +119,7 @@ thursdayDiagram.addEventListener('click', () => {
 
 fridayDiagram.addEventListener('click', () => {
     fridayDiagram.classList.toggle('active');
+    friData.classList.toggle('active');
 
     function checkClicks() {
         if (mondayDiagram.classList.contains('active') || tuesdayDiagram.classList.contains('active') || wednesdayDiagram.classList.contains('active') || thursdayDiagram.classList.contains('active') || saturdayDiagram.classList.contains('active') || sundayDiagram.classList.contains('active')) {
@@ -92,6 +129,13 @@ fridayDiagram.addEventListener('click', () => {
             thursdayDiagram.classList.remove('active');
             saturdayDiagram.classList.remove('active');
             sundayDiagram.classList.remove('active');
+            monData.classList.remove('active');
+            tueData.classList.remove('active');
+            wedData.classList.remove('active');
+            thuData.classList.remove('active');
+            satData.classList.remove('active');
+            sunData.classList.remove('active');
+            
         } else {
             return
         }
@@ -101,6 +145,7 @@ fridayDiagram.addEventListener('click', () => {
 
 saturdayDiagram.addEventListener('click', () => {
     saturdayDiagram.classList.toggle('active');
+    satData.classList.toggle('active');
 
     function checkClicks() {
         if (mondayDiagram.classList.contains('active') || tuesdayDiagram.classList.contains('active') || wednesdayDiagram.classList.contains('active') || thursdayDiagram.classList.contains('active') || fridayDiagram.classList.contains('active') || sundayDiagram.classList.contains('active')) {
@@ -110,6 +155,12 @@ saturdayDiagram.addEventListener('click', () => {
             thursdayDiagram.classList.remove('active');
             fridayDiagram.classList.remove('active');
             sundayDiagram.classList.remove('active');
+            monData.classList.remove('active');
+            tueData.classList.remove('active');
+            wedData.classList.remove('active');
+            thuData.classList.remove('active');
+            friData.classList.remove('active');
+            sunData.classList.remove('active');
         } else {
             return
         }
@@ -119,6 +170,7 @@ saturdayDiagram.addEventListener('click', () => {
 
 sundayDiagram.addEventListener('click', () => {
     sundayDiagram.classList.toggle('active');
+    sunData.classList.toggle('active');
 
     function checkClicks() {
         if (mondayDiagram.classList.contains('active') || tuesdayDiagram.classList.contains('active') || wednesdayDiagram.classList.contains('active') || thursdayDiagram.classList.contains('active') || fridayDiagram.classList.contains('active') || saturdayDiagram.classList.contains('active')) {
@@ -128,6 +180,12 @@ sundayDiagram.addEventListener('click', () => {
             thursdayDiagram.classList.remove('active');
             fridayDiagram.classList.remove('active');
             saturdayDiagram.classList.remove('active');
+            monData.classList.remove('active');
+            tueData.classList.remove('active');
+            wedData.classList.remove('active');
+            thuData.classList.remove('active');
+            friData.classList.remove('active');
+            satData.classList.remove('active');
         } else {
             return
         }
@@ -136,233 +194,17 @@ sundayDiagram.addEventListener('click', () => {
 });
 
 
-// Fetch request (AJAX does not work locally, therefore use fetch)
 
-// const test = fetch('data.json');
-
-// console.log(test);
-
-
-
-
-
-
-
-
-// regular function sequence (top to bottom no control over what logs)
-
-// function displayText(item) {
-//     document.querySelector('.test').innerHTML = item;
-// }
-
-// function myFirst() {
-//     displayText('hello');
-// }
-
-// function mySecond() {
-//     displayText('goodbye');
-// }
-
-// myFirst();
-// mySecond();
-
-
-// function sequence 
-
-// function myDisplayer(item) {
-//     document.querySelector('.test').innerHTML = item;
-//   }
-  
-//   function myCalculator(num1, num2) {
-//     let sum = num1 + num2;
-//     return sum;
-//   }
-  
-//   let result = myCalculator(10, 5);
-//   myDisplayer(result);
-
-
-// Javascript Callback Examples:
-// function myDisplayer(item) {
-//      document.querySelector('.test').innerHTML = item;
-//    }
-  
-//   function myCalculator(num1, num2, myCallback) {
-//     let sum = num1 + num2;
-//     myCallback(sum);
-//   }
-  
-//   myCalculator(10, 5, myDisplayer);
-
-
-
-
-// function greeting(name) {
-//     console.log('hello ' + name);
-// }
-
-// function myName(callback) {
-//     let name = 'Thomas';
-//     callback(name);
-// }
-
-// myName(greeting);
-
-// callback function passed into another function. 
-// simple example
-// the callback function is what is being passed through as an argument
-
-
-
-
-
-// function displayNumber(item) {
-//     console.log(item + ' callback activated');
-// }
-
-
-// function calculateNumber(num1, num2, callback) {
-//     let sum = num1 + num2;
-//     callback(sum);
-
-// }
-
-// calculateNumber(2,2,displayNumber);
-
-
-// this is a callback function for example: toggle 
-// this is a function being called for example: toggle(); first to be called!
-
-// this is a callback function: toggle = () => {}
-// this is a functon being called: toggle() WHICH DOES NOT EQUAL () => {}
-
-// callback goes through once the function being called has completed. 
-
-// function firstAction(callback, message) {
-//     console.log(message);
-//     setTimeout(callback, 2000);
-// }
-
-// function secondAction(message) {
-//     console.log(message);
-// }
-
-// setTimeout(
-//     () => firstAction(() => secondAction('i am the second action'), 'i am the first action') , 5000);
-
-
-
-
-
-// const list = ['apples', 'oranges', 'apples', 'bananas', 'melons', 'apples', 'sdasdasdasdasdas'];
-// let count = 0;
-
-
-// function runList(arr, item) {
-//     arr.forEach(item => {
-//         if (item === 'oranges') {
-//             count++;
-//         } else {
-//             return;
-//         }
-//     });
-//     console.log(`${count} ${item}`);
-// }
-
-// runList(list, 'oranges');
-
-// let longest = '';
-
-// function longestWord(arr) {
-//     for (i = 0; i < arr.length; i++) {
-//         if (arr[i].length > longest.length) longest = arr[i];
-//     }
-//     console.log(`${longest} ${longest.length}`);
-// }
-
-// longestWord(list);
-
-
-// function firstFunction(words) {
-//     console.log(`i am the first response ${words}`);
-// } 
-
-// function secondFunction() {
-//     console.log('i am the second response');
-// }
-
-
-// setTimeout(secondFunction, 5000);
-
-// setTimeout(() => firstFunction('hello world'), 3000);
-
-
-// function display(sdsd) { 
-//     console.log(sdsd);
-// }
-
-// function calculate(num1, num2, callback) {
-//     let sum = num1 + num2;
-//     let count = 10; 
-//     callback(count); // this function SWITCHES with display(sdsd); - variables overide. 
-// }
-
-// calculate(1,2,display);
-
-
-
-
-const data = ['hello', 'hello', 'apple', 'carrot', 'cigar', 'rabbit', 'rabbit', 'rabbit'];
-let count = 0;
-
-// function howMany(arr, item) {
-//     for (i = 0; i < arr.length; i++) {
-//         if (arr[i] === item) {
-//             count++;
-//         } else {
-//             return;
-//         }
-//         console.log(`${item} ${count}`);
-//     }
-
-// } 
-
-// howMany(data, 'hello');
-
-
-// function howMany(arr, item) {
-//     arr.forEach(item => {
-//         if (item === 'hello') count++;
-//     });
-//     console.log(`${item} ${count}`);
-// }
-
-// howMany(data, 'hello');
-
-// let longest = ' ';
-
-// function longestWord(arr) {
-//     for (i = 0; i < arr.length; i++) {
-//         if (arr[i].length > longest.length) longest = arr[i];
-//     }
-//     console.log(`${longest} ${longest.length}`);
-// }
-
-// longestWord(data);
-
-
-// const str = 'the big brown fox';
-
-
-// let newArray = str.split(' ');
-// let longest = ' ';
-
-
-// function runLoop(arr) {
-//     for (i = 0; i < arr.length; i++) {
-//         if (arr[i].length > longest.length) longest = arr[i];
-//     }
-//     console.log(`${longest} ${longest.length}`)
-// }
-
-// runLoop(newArray);
+// fetch data from JSON
+
+fetch('./data.json')
+.then(response => response.json())
+.then(data => {
+    monData.innerText = '$' + data[0].amount
+    tueData.innerText = '$' + data[1].amount
+    wedData.innerText = '$' + data[2].amount
+    thuData.innerText = '$' + data[3].amount
+    friData.innerText = '$' + data[4].amount
+    satData.innerText = '$' + data[5].amount
+    sunData.innerText = '$' + data[6].amount
+})
